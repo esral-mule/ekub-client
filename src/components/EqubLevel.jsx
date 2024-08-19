018d8e2f632584492cc6720b4c6f571096e380d1
Esral Desta <esraldesta@gmail.com>
2024-08-19 07:00:07 +0300

    delete equb level

--- src/components/EqubLevel.jsx
+++ src/components/EqubLevel.jsx
@@ -8,29 +8,36 @@ import {
   TableRow,
 } from "@/components/ui/table";
 import {
-    Card,
-    CardContent,
-    CardDescription,
-    CardFooter,
-    CardHeader,
-    CardTitle,
-  } from "@/components/ui/card";
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 import { useEffect, useState } from "react";
 import API from "../api/axios";
 import { useParams } from "react-router";
 import CreateEqubLevel from "./CreateEqubLevel";
-
-
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from "./ui/dropdown-menu";
+import { MoreHorizontal } from "lucide-react";
+import { Button } from "./ui/button";
+import { NavLink } from "react-router-dom";
+import BanAction from "./equb/BanAction";
+import DeleteEqubLevel from "./dialogs/DeleteEqubLevel";
+// import DeleteEqubLevel from "./DeleteEqubLevel"
 export default function EqubLevel() {
   let { id } = useParams();
 
   const [equblevels, setEqubLevels] = useState([]);
   useEffect(() => {
-    API.get(`/equb-level/etype/${id}`).then(data=>{
-        setEqubLevels(data.data.data)
-    }).catch((err)=>{
+    API.get(`/equb-level/etype/${id}`)
+      .then((data) => {
+        setEqubLevels(data.data.data);
+      })
+      .catch((err) => {
         console.log(err);
-    });
+      });
   }, []);
   return (
     <Card x-chunk="dashboard-06-chunk-0">
@@ -38,27 +45,45 @@ export default function EqubLevel() {
         {/* <Button className="self-end">Add Equb Level</Button> */}
         <CardTitle>Equb Levels</CardTitle>
         <CardDescription>List of all Equb levels</CardDescription>
-        <CreateEqubLevel fromTab={true}setEqubLevels={setEqubLevels}/>
+        <CreateEqubLevel fromTab={true} setEqubLevels={setEqubLevels} />
       </CardHeader>
       <CardContent>
-        <Table>
-          <TableHeader>
-            <TableRow>
-              <TableHead className="text-center">Title</TableHead>
-              <TableHead className="text-center">Contribution</TableHead>
-            </TableRow>
-          </TableHeader>
-          <TableBody>
-            {equblevels.map((level) => (
-              <TableRow key={level._id}>
-                <TableCell>{level.title}</TableCell>
-                <TableCell className="text-center">
-                  {level.contribution}
-                </TableCell>
+        <Card>
+          <Table>
+            <TableHeader>
+              <TableRow>
+                <TableHead className="text-center">Title</TableHead>
+                <TableHead className="text-center">Contribution</TableHead>
+                <TableHead className="text-center">Actions</TableHead>
               </TableRow>
-            ))}
-          </TableBody>
-        </Table>
+            </TableHeader>
+            <TableBody>
+              {equblevels.map((level) => (
+                <TableRow key={level._id}>
+                  <TableCell>{level.title}</TableCell>
+                  <TableCell className="text-center">
+                    {level.contribution}
+                  </TableCell>
+                  <TableCell>
+                  <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button variant="ghost" className="h-8 w-8 p-0">
+                <span className="sr-only">Open menu</span>
+                <MoreHorizontal className="h-4 w-4" />
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end">
+              <DropdownMenuLabel>Actions</DropdownMenuLabel>
+              <DropdownMenuSeparator />
+              <DeleteEqubLevel/>
+            </DropdownMenuContent>
+          </DropdownMenu>
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </Card>
       </CardContent>
       <CardFooter>
         <div className="text-xs text-muted-foreground">
---
+++ src/components/dialogs/DeleteEqubLevel.jsx
@@ -0,0 +1,110 @@
+import { Loader2 } from "lucide-react";
+import { Button } from "../ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "../ui/dialog";
+import { Card } from "../ui/card";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "../ui/table";
+import { useState } from "react";
+
+export default function DeleteEqubLevel() {
+  const [members, setMembers] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [openModal, setOpenModal] = useState(false);
+  return (
+    <Dialog open={openModal} onOpenChange={setOpenModal}>
+      <DialogTrigger asChild>
+        <Button
+          size="xs"
+          onClick={() => {
+            setOpenModal(true);
+          }}
+          className="ml-auto print:hidden bg-green-700 mr-1 w-full"
+        >
+          Delete
+        </Button>
+      </DialogTrigger>
+      <DialogContent className="sm:max-w-[425px]">
+        <DialogHeader>
+          <DialogTitle className="flex items-center">
+            <p className="pl-1">Delete Equb level</p>
+          </DialogTitle>
+          <DialogDescription>
+            Please make sure you want to delete{" "}
+          </DialogDescription>
+        </DialogHeader>
+        <div>
+          {loading ? (
+            <div className="flex justify-center">
+              <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+            </div>
+          ) : (
+            members && (
+              <Card>
+                {members.length > 0 ? (
+                  <Table>
+                    <TableHeader>
+                      <TableRow>
+                        <TableHead>Full Name</TableHead>
+                        <TableHead>Level</TableHead>
+                        <TableHead className="text-right">Amount</TableHead>
+                      </TableRow>
+                    </TableHeader>
+                    <TableBody>
+                      {members.map((detail) => (
+                        <TableRow key={detail._id}>
+                          <TableCell className="font-medium">
+                            {detail.member.fullName}
+                          </TableCell>
+                          <TableCell>{detail.equbLevel.title}</TableCell>
+                          <TableCell className="text-right">
+                            {detail.equbLevel.contribution}
+                          </TableCell>
+                        </TableRow>
+                      ))}
+                    </TableBody>
+                  </Table>
+                ) : (
+                  <div className="text-center">
+                    No Member is assigned to this UniqueId
+                  </div>
+                )}
+              </Card>
+            )
+          )}
+        </div>
+        <DialogFooter>
+        <Button
+            size="sm"
+            className="h-8 gap-1 bg-green-600"
+            // onClick={handleSubmit}
+            type="submit"
+          >
+            <span>Cancel</span>
+          </Button>
+          <Button
+            size="sm"
+            className="h-8 gap-1 bg-red-600"
+            // onClick={handleSubmit}
+            type="submit"
+          >
+            <span>Delete</span>
+          </Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+}
