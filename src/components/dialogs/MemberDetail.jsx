fec7fb2de3001bc498429e4b4c151878428501ca
Esral Desta <esraldesta@gmail.com>
2024-08-21 11:29:38 +0300

    color fix

--- src/components/dialogs/MemberDetail.jsx
+++ src/components/dialogs/MemberDetail.jsx
@@ -19,42 +19,42 @@ export default function MemberDetail({ user }) {
           Detail
         </Button>
       </DialogTrigger>
-      <DialogContent className="sm:max-w-[425px] p-6 bg-white rounded-md shadow-lg">
+      <DialogContent className="sm:max-w-[425px] p-6 rounded-md shadow-lg">
         <DialogHeader>
-          <DialogTitle className="text-lg font-semibold text-gray-900">
+          <DialogTitle className="text-lg font-semibold">
             Member Detail
           </DialogTitle>
-          <DialogDescription className="text-sm text-gray-600">
+          <DialogDescription className="text-sm">
             Detailed information about the selected member.
           </DialogDescription>
         </DialogHeader>
         <div className="space-y-4 max-h-52 overflow-y-scroll">
           <div className="border-b pb-2 flex items-center flex-wrap gap-2">
-            <p className="text-sm font-bold text-gray-700">Full Name:</p>
-            <p className="text-md text-gray-900">{user.member.fullName}</p>
+            <p className="text-sm font-bold">Full Name:</p>
+            <p className="text-md">{user.member.fullName}</p>
           </div>
           <div className="border-b pb-2 flex items-center flex-wrap gap-2">
-            <p className="text-sm font-bold text-gray-700">Phone Number:</p>
-            <p className="text-md text-gray-900">{user.member.phoneNumber}</p>
+            <p className="text-sm font-bold ">Phone Number:</p>
+            <p className="text-md ">{user.member.phoneNumber}</p>
           </div>
           <div className="border-b pb-2 flex items-center flex-wrap gap-2">
-            <p className="text-sm font-bold text-gray-700">Username:</p>
-            <p className="text-md text-gray-900">{user.member.username}</p>
+            <p className="text-sm font-bold ">Username:</p>
+            <p className="text-md ">{user.member.username}</p>
           </div>
           <div className="border-b pb-2 flex items-center flex-wrap gap-2">
-            <p className="text-sm font-bold text-gray-700">Equb Type:</p>
-            <p className="text-md text-gray-900">{user.equbType.name}</p>
+            <p className="text-sm font-bold ">Equb Type:</p>
+            <p className="text-md ">{user.equbType.name}</p>
           </div>
           <div className="border-b pb-2 flex items-center flex-wrap gap-2">
-            <p className="text-sm font-bold text-gray-700">Equb Level:</p>
-            <p className="text-md text-gray-900">{user.equbLevel.title}</p>
+            <p className="text-sm font-bold ">Equb Level:</p>
+            <p className="text-md ">{user.equbLevel.title}</p>
           </div>
           <div className="border-b pb-2 flex items-center flex-wrap gap-2">
-            <p className="text-sm font-bold text-gray-700">Unique ID:</p>
-            <p className="text-md text-gray-900">{user.uniqueId.uniqueId}</p>
+            <p className="text-sm font-bold ">Unique ID:</p>
+            <p className="text-md ">{user.uniqueId.uniqueId}</p>
           </div>
           <div className="flex items-center flex-wrap gap-2">
-            <p className="text-sm font-bold text-gray-700">Active Status:</p>
+            <p className="text-sm font-bold">Active Status:</p>
             <p
               className={`text-md ${
                 user.isActive ? "text-green-600" : "text-red-600"
