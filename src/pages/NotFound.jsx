e077a126cac639e9b9c8787270e1f9bb9520537f
Esral Desta <esraldesta@gmail.com>
2024-08-26 14:54:00 +0300

    not found

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,9 +1,21 @@
-import { Card } from "../components/ui/card";
+import { Link } from "react-router-dom";
+import { Button } from "../components/ui/button";
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "../components/ui/card";
 
 export default function NotFound() {
   return (
-    <div className="min-h-[200px] flex justify-center items-center">
-      Page not Found
-    </div>
+    <Card className="w-full max-w-sm mx-auto">
+      <CardHeader>
+        <CardTitle className="text-2xl">404</CardTitle>
+        <CardDescription>
+          Page Not Found
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+      </CardContent>
+      <CardFooter>
+        <Button className="w-full"><Link to="/">back to home</Link></Button>
+      </CardFooter>
+    </Card>
   );
 }
