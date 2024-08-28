7c8a107f38aede46dc54089511a21e7065b0e812
Esral Desta <esraldesta@gmail.com>
2024-08-28 12:29:58 +0300

    icons

--- src/pages/Support.jsx
+++ src/pages/Support.jsx
@@ -1,12 +1,35 @@
-import { useTranslation } from "react-i18next";
+import { useRef } from 'react';
+import emailjs from '@emailjs/browser';
 
-export default function Support() {
-    const { t} = useTranslation("global");
+export default function Support () {
+  const form = useRef();
 
-    return (
-    <div>
-        <p>Support</p>
-        <p>{t("coming.message")}</p>
-    </div>
-  )
-}
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm('service_tirfu78', 'template_8bmo1e7', form.current, {
+        publicKey: 'VW4wdWE-aTFNYn3Qs',
+      })
+      .then(
+        () => {
+          console.log('SUCCESS!');
+        },
+        (error) => {
+          console.log('FAILED...', error.text);
+        },
+      );
+  };
+
+  return (
+    <form ref={form} onSubmit={sendEmail}>
+      <label>Name</label>
+      <input type="text" name="user_name" />
+      <label>Email</label>
+      <input type="email" name="user_email" />
+      <label>Message</label>
+      <textarea name="message" />
+      <input type="submit" value="Send" />
+    </form>
+  );
+};
