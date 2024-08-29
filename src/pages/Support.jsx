78ed9d40caba3de06f64fbb4f850e65d7af6ede0
Esral Desta <esraldesta@gmail.com>
2024-08-29 07:10:08 +0300

    support page

--- src/pages/Support.jsx
+++ src/pages/Support.jsx
@@ -3,36 +3,30 @@ import emailjs from "@emailjs/browser";
 import { Label } from "../components/ui/label";
 import { Textarea } from "../components/ui/textarea";
 import { Input } from "../components/ui/input";
-import { Card } from "../components/ui/card";
 import { Button } from "../components/ui/button";
-import Transition from "../components/Transition";
 
 export default function Support() {
-  const [isLoading,setIsLoading] = useState(false);
   const form = useRef();
   const [formData, setFormData] = useState({
-    name: "",
-    email: "",
+    from_name: "",
+    user_name: "",
     message: "",
   });
   const [errors, setErrors] = useState({});
 
   const validate = () => {
     let formErrors = {};
-    
-    // Name validation
-    if (!formData.name) {
-      formErrors.name = "Name is required";
+
+    if (!formData.user_name) {
+      formErrors.user_name = "Name is required";
     }
 
-    // Email validation
-    if (!formData.email) {
-      formErrors.email = "Email is required";
-    } else if (!/\S+@\S+\.\S+/.test(formData.email)) {
-      formErrors.email = "Email address is invalid";
+    if (!formData.from_name) {
+      formErrors.from_name = "Email is required";
+    } else if (!/\S+@\S+\.\S+/.test(formData.from_name)) {
+      formErrors.from_name = "Email address is invalid";
     }
 
-    // Message validation
     if (!formData.message) {
       formErrors.message = "Message is required";
     }
@@ -50,12 +44,9 @@ export default function Support() {
 
   const sendEmail = (e) => {
     e.preventDefault();
-    // Validate before submitting
+
     const formErrors = validate();
     if (Object.keys(formErrors).length === 0) {
-      // No validation errors, send email
-    setIsLoading(true)
-
       emailjs
         .sendForm(
           "service_tirfu78",
@@ -65,59 +56,61 @@ export default function Support() {
         )
         .then(
           () => {
-            setIsLoading(false)
             console.log("SUCCESS!");
           },
           (error) => {
-            setIsLoading(false)
             console.log("FAILED...", error.text);
           }
         );
     } else {
       setErrors(formErrors);
-      setIsLoading(false)
     }
   };
 
   return (
-    <Transition>
-
     <form
       ref={form}
       onSubmit={sendEmail}
-      className="max-w-lg mx-auto mt-5"
+      className="rounded-lg border p-3 w-full sm:w-[500px] mx-auto mt-2"
     >
-      <div className="space-y-1 text-left">
-        {errors.name && <p className="text-red-500 text-sm">{errors.name}</p>}
-        <Label htmlFor="name" className="w-16">
+      <div className="flex items-center gap-2 my-1">
+        <Label htmlFor="user_name" className="w-16">
           Name
         </Label>
         <Input
-          id="name"
+          id="user_name"
           type="text"
           placeholder="Your name"
-          name="name"
-          value={formData.name}
+          name="user_name"
+          value={formData.user_name}
           onChange={handleInputChange}
         />
+        {errors.user_name && (
+          <p className="text-red-500 text-sm">{errors.user_name}</p>
+        )}
       </div>
 
-      <div className="space-y-1 text-left">
-        {errors.email && <p className="text-red-500 text-sm">{errors.email}</p>}
-        <Label htmlFor="email" className="w-16">Email</Label>
+      <div className="flex items-center gap-2 my-1">
+        <Label htmlFor="from_name" className="w-16">
+          Email
+        </Label>
         <Input
-          id="email"
+          id="from_name"
           type="email"
           placeholder="Your email"
-          name="email"
-          value={formData.email}
+          name="from_name"
+          value={formData.from_name}
           onChange={handleInputChange}
         />
+        {errors.from_name && (
+          <p className="text-red-500 text-sm">{errors.from_name}</p>
+        )}
       </div>
 
-      <div className="space-y-1 text-left">
-        {errors.message && <p className="text-red-500 text-sm">{errors.message}</p>}
-        <Label htmlFor="message" className="w-16">Message</Label>
+      <div className="flex items-center gap-2 my-1">
+        <Label htmlFor="message" className="w-16">
+          Message
+        </Label>
         <Textarea
           id="message"
           name="message"
@@ -126,12 +119,16 @@ export default function Support() {
           value={formData.message}
           onChange={handleInputChange}
         />
+        {errors.message && (
+          <p className="text-red-500 text-sm">{errors.message}</p>
+        )}
       </div>
 
       <div className="flex justify-end">
-        <Button type="submit" className="mx-1 mt-3"> {isLoading?"Sending":"Send Email"}</Button>
+        <Button type="submit" className="m-1">
+          Send Email
+        </Button>
       </div>
     </form>
-    </Transition>
   );
 }
