926959442e2507d2b8c7d3a6d294b32b42ff289f
Esral Desta <esraldesta@gmail.com>
2024-09-25 16:14:01 +0300

    icons

--- vite.config.js
+++ vite.config.js
@@ -37,13 +37,13 @@ const manifestForPlugIn = {
       },
       {
         src: "/icons/android/android-launchericon-192-192.png",
-        sizes: "144x144",
+        sizes: "192x192",
         type: "image/png",
         purpose: "maskable any",
       },
       {
         src: "/icons/android/android-launchericon-512-512.png",
-        sizes: "144x144",
+        sizes: "512x512",
         type: "image/png",
         purpose: "maskable any",
       },
